1f88448c2b6e4f1e373d5ef96fa64372f1ef2157
K-Nick <kningtg@gmail.com>
2018-02-20 22:49:53 +0800

    add photos

--- tool.py
+++ tool.py
@@ -51,7 +51,7 @@ def compress(choose, des_dir, src_dir, file_list):
     
     参数
     -----------
-    choose: '4'
+    choose: '1'
             选择压缩的比例，有4个选项，越大压缩后的图片越小
     """
     if choose == '1':
@@ -139,6 +139,7 @@ def cut_photo():
     """
     src_dir = "photos/"
     if directory_exists(src_dir):
+        print("find!")
         if not directory_exists(src_dir):
             make_directory(src_dir)
         # business logic
